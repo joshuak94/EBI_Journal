d9a5a22e8ed21655972536c7d38f3a881fb7f63e
Josh Kim <33100114+joshuak94@users.noreply.github.com>
2019-05-07 13:31:50 +0100

    Add two more days, update scripts.

--- scripts/wiggletools_max_shift.py
+++ scripts/wiggletools_max_shift.py
@@ -1,6 +1,7 @@
 import argparse
 import subprocess
 import sys
+import os
 from multiprocessing import Pool
 
 pos_file = ""
@@ -12,6 +13,16 @@ def pearson(shift):
                          stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
     out, err = p.communicate()
     return float(out.rstrip())
+
+# For wigCorrelate
+def wigCorrelate(shift):
+    print("Processing %d" % shift)
+    tmpfile = "/scratch/joshjayk/shift%d.wig" % shift
+    command = "wiggletools write %s shiftPos %s %s;../../wigCorrelate %s %s;rm %s" % (tmpfile, str(shift), neg_file, pos_file, tmpfile, tmpfile)
+    p = subprocess.Popen(command, stdout = subprocess.PIPE, stderr = subprocess.STDOUT, shell = True)
+    out, err = p.communicate()
+    print(out.split("\t"))
+    return float(out.split("\t")[2].rstrip())
 
 def pearson_masc(shift):
     print("Processing %d" % shift)
@@ -26,6 +37,7 @@ if __name__ == '__main__':
     parser.add_argument("-e", "--end", type = int, default = 400, help = "The final shift value to calculate.")
     parser.add_argument("-s", "--step", type = int, default = 1, help = "The step size for shift values.")
     parser.add_argument("-m", "--masc", action = "store_true", help = "Use MaSC formula for calculating neg strand coverage.")
+    parser.add_argument("-w", "--wigCorrelate", action = "store_true", help = "Use wigCorrelate to calculate pearson values.")
     parser.add_argument("-p", "--pos", type = str, help = "The name of the positive strand file.", required = True)
     parser.add_argument("-n", "--neg", type = str, help = "The name of the negative strand file.", required = True)
     parser.add_argument("-o", "--out", type = str, help = "The name of the file to save to.", required = True)
@@ -38,8 +50,11 @@ if __name__ == '__main__':
     if args.masc:
         print("MaSC set.")
         pearson_vals = pool.map(pearson_masc, range(args.initial, args.end + args.step, args.step))
+    elif args.wigCorrelate:
+        print("wigCorrelate set.")
+        pearson_vals = pool.map(wigCorrelate, range(args.initial, args.end + args.step, args.step))
     else:
-        print("MaSC not set.")
+        print("Wiggletools set.")
         pearson_vals = pool.map(pearson, range(args.initial, args.end + args.step, args.step))
     print("Max pearson value: %2.6f\n" % (max(pearson_vals)))
     print("Corresponding shift: %d\n" % (pearson_vals.index(max(pearson_vals)) + 1))
